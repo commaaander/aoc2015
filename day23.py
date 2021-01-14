e6e96b014e5a978bfc6854990bf8ed87446035b7
Ewoud Dronkert <ewoud@dronkert.net>
2021-01-14 02:02:50 +0100

    shorter VM

--- day23.py
+++ day23.py
@@ -7,28 +7,22 @@ with open('input23.txt') as f:
         if m := instr.match(line.strip()):
             mem.append(m.groupdict())
             if mem[-1]['off'] is not None:
-                mem[-1]['off'] = int(mem[-1]['off'])
+                mem[-1]['off'] = int(mem[-1]['off']) - 1
 
 def run(a):
     reg = {'a': a, 'b': 0}
     ip = 0
     while ip >= 0 and ip < len(mem):
-        a = mem[ip]
-        if a['op'] == 'inc':
-            reg[a['reg']] += 1
-            ip += 1
-        elif a['op'] == 'hlf':
-            reg[a['reg']] //= 2
-            ip += 1
-        elif a['op'] == 'tpl':
-            reg[a['reg']] *= 3
-            ip += 1
-        elif a['op'] == 'jmp':
-            ip += a['off']
-        elif a['op'] == 'jie':
-            ip += a['off'] if reg[a['reg']] % 2 == 0 else 1
-        elif a['op'] == 'jio':
-            ip += a['off'] if reg[a['reg']] == 1 else 1
+        i, r, j = mem[ip].values()
+        if i == 'inc':
+            reg[r] += 1
+        elif i == 'hlf':
+            reg[r] //= 2
+        elif i == 'tpl':
+            reg[r] *= 3
+        elif i == 'jmp' or (i == 'jie' and reg[r] % 2 == 0) or (i == 'jio' and reg[r] == 1):
+            ip += j
+        ip += 1
     return reg['b']
 
 print(run(0), run(1))
