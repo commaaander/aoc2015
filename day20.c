32b5955788889879f78b9b4b5f77d934cc546d84
E. Dronkert <ednl@users.noreply.github.com>
2021-09-15 07:59:32 +0200

    naming

--- day20.c
+++ day20.c
@@ -4,19 +4,19 @@
 #define N   1000000  // number of houses (arbitrary)
 #define M1       10  // elf multiplier (part 1)
 #define M2       11  // elf multiplier (part 2)
-#define LIM      50  // max house visits (part 2)
+#define Q        50  // max house visits (part 2)
 
 int presents[N] = {M1};  // init for part 1
 
 int main(void)
 {
-    int elf, house, pres, visit;
+    int elf, house, npres, visit;
 
     // Part 1: distribute presents (elf 1 already visited by init)
     for (elf = 2; elf < N; ++elf) {
-        pres = elf * M1;
+        npres = elf * M1;
         for (house = elf; house < N; house += elf) {
-            presents[house] += pres;
+            presents[house] += npres;
         }
     }
 
@@ -28,18 +28,15 @@ int main(void)
         }
     }
 
-    // Part 2: reset
+    // Part 2: distribute presents
     for (house = 1; house < N; ++house) {
         presents[house] = M2;
     }
-
-    // Part 2: distribute presents
-    // Elf 1 already visited by reset
     for (elf = 2; elf < N; ++elf) {
-        pres = elf * M2;
+        npres = elf * M2;
         visit = 0;
-        for (house = elf; visit++ < LIM && house < N; house += elf) {
-            presents[house] += pres;
+        for (house = elf; visit++ < Q && house < N; house += elf) {
+            presents[house] += npres;
         }
     }
 
